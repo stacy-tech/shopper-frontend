5872dcf435449e6b5ec24a40772cca5cf21c9ef6
stacy <stacydn23@gmail.com>
2021-11-23 08:14:50 -0500

    add redux as well as react-redux to my package-lock. also structure a reducer file and imoort it to my index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import reducer from './redux/reducer'
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    products: []
+}
+
+export default function reducer(state=initialState, action){
+    switch (action.type){
+        case "GET_PRODUCTS":
+            return {...state, products: action.payload};
+        default:
+            return {...state}
+    }
+    
+}
