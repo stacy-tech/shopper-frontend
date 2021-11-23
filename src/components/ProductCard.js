523cb521b2e681cc03f3ea0a3fa1a66a038d89f5
stacy <stacydn23@gmail.com>
2021-11-23 18:37:59 -0500

    update my css as well as my product card div order

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,7 @@
 export default function ProductCard({title, image_url, price}){
     return <div className="card">
-        <h3>{title}</h3>
         <img src={image_url} alt={title}/>
-        <p>{price}</p>
+        <h3>{title}</h3>
+        <h3>${price}</h3>
     </div>
 }
