cea21c40fb7821aebeda40f8a790b8f71f7bfda5
stacy <stacydn23@gmail.com>
2021-11-25 16:55:32 -0500

    structure a link inside a div in my productshow which will be able to help clients go to the next item/product. i also import link from react-router-dom, as well as updated my css

--- src/components/ProductShow.js
+++ src/components/ProductShow.js
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getProduct } from '../redux/actionCreators'
 import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
 function ProductShow({getProduct}){
     const routeId = useParams().id
@@ -9,7 +10,10 @@ function ProductShow({getProduct}){
         getProduct(routeId)
     },     [getProduct, routeId])
     
-    return <h1>You're in the show page</h1>
+    return  <div>
+        <h1>You're in the show page</h1>
+        <Link to={`/products/${parseInt(routeId) + 1}`}>Next</Link>
+    </div>
 }
 
 export default connect(null, {getProduct})(ProductShow)
