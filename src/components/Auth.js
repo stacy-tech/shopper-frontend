4e5d57241264690b8038e84a30da03a8cf14ba89
stacy <stacydn23@gmail.com>
2021-11-26 19:34:31 -0500

    add a react input form for my sign up as well as a css style for my input

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,11 +2,30 @@ import { useState } from 'react'
 
 function Auth(props){
     const [signup, setSignup] =  useState(false)
-
+    
     const toggleSignup = () => setSignup(!signup)
 
     return <>
         {signup ? <h1>Sign up!</h1> :<h1>Login</h1>}
+        <form>
+            <label>
+                First Name:
+                <input type="text" name="first_name" />
+            </label>
+            <label>
+                Last Name:
+                <input type="text" name="last_name" />
+            </label>
+            <label>
+                Username:
+                <input type="text" name="username" />
+            </label>
+            <label>
+                Password:
+                <input type="password" name="password" />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
         <button onClick={toggleSignup}>or... {signup ? "login!" : "signup!"}</button>
     </>
     
