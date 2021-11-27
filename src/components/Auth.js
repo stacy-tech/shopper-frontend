2a8796f7d6e57dc78f8a0fe5705ceea4f0471462
stacy <stacydn23@gmail.com>
2021-11-26 22:01:07 -0500

    structure a controlled formed which is able to stimmulate whether a new user is signing up or a existing user is returning and able to sign in

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,27 +2,31 @@ import { useState } from 'react'
 
 function Auth(props){
     const [signup, setSignup] =  useState(false)
+    const [first_name, setFirst_name] = useState("")
+    const [last_name, setLast_name] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
     
     const toggleSignup = () => setSignup(!signup)
 
     return <>
         {signup ? <h1>Sign up!</h1> :<h1>Login</h1>}
         <form>
-            <label>
+            {signup && <label>
                 First Name:
-                <input type="text" name="first_name" />
-            </label>
-            <label>
+                <input type="text" name="first_name" value={first_name} onChange={(e) => setFirst_name(e.target.value)} />
+            </label>}
+            {signup && <label>
                 Last Name:
-                <input type="text" name="last_name" />
-            </label>
+                <input type="text" name="last_name" value={last_name} onChange={(e) => setLast_name(e.target.value)} />
+            </label>}
             <label>
                 Username:
-                <input type="text" name="username" />
+                <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
             </label>
             <label>
                 Password:
-                <input type="password" name="password" />
+                <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             </label>
             <input type="submit" value="Submit" />
         </form>
