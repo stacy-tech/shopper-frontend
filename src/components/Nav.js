130967f2879136366e2d708a55ec2b989f04e08e
stacy <stacydn23@gmail.com>
2021-12-02 23:00:19 -0500

    structure a if/else statment to verify whether a user is logged in oe not

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,11 +2,17 @@ import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { logout } from '../redux/actionCreators'
 
-function Nav({logout}){
-    return <nav>
-        <NavLink to="/products"><button>See All Items</button></NavLink>
-        <button onClick={logout}>Logout</button>
-    </nav>
+function Nav({logout, username}){
+    if(username){
+        return <nav>
+            <NavLink to="/products"><button>See All Items</button></NavLink>
+            <button onClick={logout}>Logout</button>
+        </nav>
+   } else {
+       return <nav>log in / register </nav>
+   }
 }
 
-export default connect(null, {logout})(Nav)
+const mapStateToProps = (state) => ({username: state.user.username})
+
+export default connect(mapStateToProps, {logout})(Nav)
